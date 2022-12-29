1611e6b7128469ad9937b0c7ebac69539789aa8d
yeonsu <drawingbonny@gmail.com>
2022-12-29 12:34:36 +0900

    feat: Add input objects

---
+++ area-calculator/area-calculator/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  area-calculator
+//
+//  Created by yeonsu on 2022/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
