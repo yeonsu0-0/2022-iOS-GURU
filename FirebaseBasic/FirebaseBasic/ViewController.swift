8013759f40d71b2e3ce754712f2e3c59bbabfef1
yeonsu <drawingbonny@gmail.com>
2023-01-10 17:38:32 +0900

    feat: Add firebase authentication

---
+++ FirebaseBasic/FirebaseBasic/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  FirebaseBasic
+//
+//  Created by yeonsu on 2023/01/10.
+//
+
+import UIKit
+import FirebaseAuthUI
+import FirebaseEmailAuthUI
+
+class ViewController: UIViewController, FUIAuthDelegate {
+
+    var handle:AuthStateDidChangeListenerHandle!
+    
+    let authUI = FUIAuth.defaultAuthUI()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        authUI!.delegate = self
+    }
+    
+    /* 인증 상태 수신 대기 */
+    // 로그인한 사용자에 대한 정보가 필요한 앱의 각 뷰에 대해 리스너를 FIRAuth 개체에 연결
+    // 이 리스너는 사용자의 로그인 상태가 변경될 때마다 호출
+    
+    override func viewWillAppear(_ animated: Bool) {
+        handle = Auth.auth().addStateDidChangeListener { auth, user in
+          
+            // 로그인 유무 확인
+            if let currentUser = auth.currentUser {
+                // 로그인 상태
+                NSLog("Logged In")
+            } else {
+                // 로그아웃 상태
+                NSLog("Logged Out")
+                let providers: [FUIAuthProvider] = [
+                  FUIEmailAuth()
+                ]
+                self.authUI!.providers = providers
+                let authVC = self.authUI!.authViewController()
+                authVC.modalPresentationStyle = .fullScreen
+                self.present(authVC, animated: false, completion: nil)
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        Auth.auth().removeStateDidChangeListener(handle!)
+        // 다른 화면으로 넘어갈 때 리스너 제거
+    }
+
+
+}
+
